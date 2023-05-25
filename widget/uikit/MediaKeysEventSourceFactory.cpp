8c8935a2137bc122123c902fee91b62da363c0bb
Nika Layzell <nika@thelayzells.com>
2023-05-26 06:04:44 +0900

    Stub out MediaKeysEventSourceFactory

---
+++ widget/uikit/MediaKeysEventSourceFactory.cpp
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "MediaKeysEventSourceFactory.h"
+
+#include "mozilla/Assertions.h"
+
+namespace mozilla {
+namespace widget {
+
+mozilla::dom::MediaControlKeySource* CreateMediaControlKeySource() {
+  MOZ_CRASH("UNIMPLEMENTED");
+}
+
+}  // namespace widget
+}  // namespace mozilla
