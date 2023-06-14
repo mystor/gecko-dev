6358cb1ea7239d1bb72b0535bb2edf7c3e04472d
Nika Layzell <nika@thelayzells.com>
2023-06-14 18:15:05 -0400

    Shim out nsNativeTheme on iOS

--- widget/uikit/nsNativeThemeUIKit.mm
+++ widget/uikit/nsNativeThemeUIKit.mm
@@ -3,9 +3,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "nsNativeTheme.h"
-#include "Theme.h"
+#include "nsNativeThemeUIKit.h"
 
-already_AddRefed<mozilla::widget::Theme> do_CreateNativeThemeDoNotUseDirectly() {
-  MOZ_CRASH("UNIMPLEMENTED");
+using namespace mozilla;
+using namespace mozilla::gfx;
+using namespace mozilla::widget;
+
+nsNativeThemeUIKit::nsNativeThemeUIKit() : Theme(ScrollbarStyle()) {}
+
+already_AddRefed<Theme> do_CreateNativeThemeDoNotUseDirectly() {
+  if (gfxPlatform::IsHeadless()) {
+    return do_AddRef(new Theme(Theme::ScrollbarStyle()));
+  }
+  return do_AddRef(new nsNativeThemeUIKit());
 }
