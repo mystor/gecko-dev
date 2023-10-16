61d2b8223e0e3d373d35a9fd132b19ca40290cb2
Nika Layzell <nika@thelayzells.com>
2023-10-16 12:58:03 -0400

    Bug 1859357 - Part 2: Replace ExampleBrowser with a project using the new API, r=glandium!
    This is a project with very similar functionality to the existing
    ExampleBrowser project, however built using the new GeckoView-style embedding
    API introduced in the previous part. Like the ExampleBrowser project, this is
    built using xcode rather than the normal build system, however basic building,
    signing & packaging is now performed automatically by the Makefile when
    building on a macOS host.
    
    In another patch, I have a set-up which builds the application bundle manually
    within the Makefile, avoiding any xcode dependency. Unfortunately this makes it
    more difficult to run on a real iOS device due to the need for manual signing
    and provisioning profiles.
    
    Differential Revision: https://phabricator.services.mozilla.com/D191102

--- mobile/ios/TestRunner/TestRunner/EmbedGecko.sh
+++ mobile/ios/TestRunner/TestRunner/EmbedGecko.sh
@@ -6,15 +6,21 @@
 set -e
 
 if test "${ACTION}" != "clean"; then
-    echo "Building in ${GECKO_OBJDIR}"
-    make -j8 -s -C $GECKO_OBJDIR binaries
+    TOPSRCDIR="${SRCROOT}/../../.."
 
-    echo "Copying files from ${GECKO_OBJDIR}/dist/bin"
+    if [ -z "${TOPOBJDIR}" ]; then
+        TOPOBJDIR=`"${TOPSRCDIR}/mach" environment --format=json | python3 -c 'import sys, json; print(json.load(sys.stdin)["topobjdir"])'`
+        if [ -z "${TOPOBJDIR}" ]; then
+            echo "Error: Could not determine TOPOBJDIR"
+        fi
+    fi
+
+    echo "Copying files from ${TOPOBJDIR}/dist/bin"
     echo "Copying files to $BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Frameworks"
     rsync -pvtrlL --exclude "Test*" \
           --exclude "test_*" --exclude "*_unittest" \
           --exclude xulrunner  \
-          ${GECKO_OBJDIR}/dist/bin/ $BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Frameworks
+          "${TOPOBJDIR}/dist/bin/" "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Frameworks"
 
     echo "${__IS_NOT_SIMULATOR}"
     if test "${__IS_NOT_SIMULATOR}" = "YES"; then
